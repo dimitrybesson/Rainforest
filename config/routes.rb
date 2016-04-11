fe2864c751d0171a6178e4e850dfb8a0c3e5dc8d
Dimitry Besson <dimitrybesson@gmail.com>
2016-04-11 16:03:55 -0400

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'users/new'
 
   get 'users/create'
@@ -14,6 +20,7 @@ Rails.application.routes.draw do
   # get 'products/edit'
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
